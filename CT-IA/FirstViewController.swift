05c8b41bb75ffcae15a07df8affa43ce5c73a9be
euisangs2020 <638hb77p>
2019-06-11 12:26:11 +0800

    Initial Commit

---
+++ CT-IA/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  CT-IA
+//
+//  Created by Ethan Shin on 11/6/2019.
+//  Copyright © 2019 Ethan Shin. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CT-IA/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  CT-IA
+//
+//  Created by Ethan Shin on 11/6/2019.
+//  Copyright © 2019 Ethan Shin. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
